b4a87b7db731ade39941f18e02f4e30c528aea99
onesick <onesick@iium.kr>
2023-07-06 11:37:22 +0900

    게시판 상세 스타일 완료
    -문의하기 상세에 작업

--- www/sub/_contact_view.php
+++ www/sub/_contact_view.php
@@ -15,16 +15,53 @@ $inq = sql_fetch(" select * from {$inquery_table} where inq_id='{$idx}' ");
       <div class="container">
         <div class="wrapper">
           <div class="sec_ct">
-            제목 : <?php echo $inq['inq_subj']; ?> <br/><br/>
-            날짜 : <?php echo date("Y.m.d", strtotime($inq['inq_date'])); ?> <br/><br/>
-            이름 : <?php echo $inq['inq_name']; ?> <br/><br/>
-            이메일 : <?php echo $inq['inq_mail']; ?> <br/><br/>
-            지역 : <?php echo $inq['inq_area']; ?> <br/><br/>
-            회사/학교 : <?php echo $inq['inq_company']; ?> <br/><br/>
-            부서 : <?php echo $inq['inq_depart']; ?> <br/><br/>
-            전화번호 : <?php echo $inq['inq_tel']; ?> <br/><br/>
-            주소 : <?php echo $inq['inq_add']; ?> <br/><br/>
-            문의사항 : <?php echo nl2br($inq['inq_content']); ?>
+            <div class="board-head">
+              <p class="board-v-subj"><?php echo $inq['inq_subj']; ?></p>
+              <p class="board-v-date"><?php echo date("Y.m.d", strtotime($inq['inq_date'])); ?></p>
+            </div>
+            <div class="board-v-cont">
+              <div class="inq-v-text_wrap">
+                <p class="match-height inq-v-text">
+                  <span class="inq-v-text-tit">이름</span>
+                  <span class="inq-v-text-cont"><?php echo $inq['inq_name']; ?></span>
+                </p>
+                <p class="match-height inq-v-text">
+                  <span class="inq-v-text-tit">이메일</span>
+                  <span class="inq-v-text-cont"><?php echo $inq['inq_mail']; ?></span>
+                </p>
+                <p class="match-height inq-v-text">
+                  <span class="inq-v-text-tit">지역</span>
+                  <span class="inq-v-text-cont"><?php echo $inq['inq_area']; ?></span>
+                </p>
+                <p class="match-height inq-v-text">
+                  <span class="inq-v-text-tit">회사/학교</span>
+                  <span class="inq-v-text-cont"><?php echo $inq['inq_company']; ?></span>
+                </p>
+                <p class="match-height inq-v-text">
+                  <span class="inq-v-text-tit">부서</span>
+                  <span class="inq-v-text-cont"><?php echo $inq['inq_depart']; ?></span>
+                </p>
+                <p class="match-height inq-v-text">
+                  <span class="inq-v-text-tit">전화번호</span>
+                  <span class="inq-v-text-cont"><?php echo $inq['inq_tel']; ?></span>
+                </p>
+                <p class="match-height inq-v-text">
+                  <span class="inq-v-text-tit">주소</span>
+                  <span class="inq-v-text-cont"><?php echo $inq['inq_add']; ?></span>
+                </p>
+                <p class="match-height inq-v-text">
+                  <span class="inq-v-text-tit">문의사항</span>
+                  <span class="inq-v-text-cont"><?php echo nl2br($inq['inq_content']); ?></span>
+                </p>
+              </div>
+            </div>
+            <div class="board-v-btn_group">
+              <a href="" class="board-v-golist-btn">목록</a>
+              <div class="board-v-navi_group">
+                <a href="" class="board-v-navi-btn prev"><span class="icon"></span>PREV</a>
+                <a href="" class="board-v-navi-btn next">NEXT<span class="icon"></span></a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
