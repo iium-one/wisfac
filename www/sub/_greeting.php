de8605e7395ffb62d3b19429cc4e3cd12de4ad7f
onesick <onesick@iium.kr>
2023-07-10 11:39:41 +0900

    (위스팩소개>CEO인사말) PC 완료

--- www/sub/_greeting.php
+++ www/sub/_greeting.php
@@ -4,4 +4,40 @@ include_once(G5_INCLUDE_PATH.'/sub_top.php');
 
 <div id="greeting" class="contents">
   <?php sub_top($sb_menus, 'introduce', 'greeting'); ?>
+
+  <div id="sb-contents">
+    <section class="greeting-intro">
+      <h2 class="sound_only">비즈니스 소개 영역</h2>
+      <div class="container">
+        <div class="wrapper">
+          <div class="sec_ct">
+            <div class="greeting-lt">
+              <div class="sb_top-title">
+                <p class="sb_top-title-text1">반도체용 웨이퍼 <br/>검사 시스템 시장을 선도하는 <br/><span class="highlight"><b>기술집약형 세계 일류 기업</b></span></p>
+              </div>
+              <div class="greeting-ct">
+                <div class="greeting-text-box">
+                  <p class="greeting-text">고객 여러분의 관심에 깊은 감사를 드리며 위스팩을 찾아주신 여러분을 환영합니다.</p>
+                  <p class="greeting-text">위스팩은 반도체용 웨이퍼 검사 시스템 시장을 선도하는 기술집약형 세계 일류 기업을 목표로 2018년에 설립되었습니다.</p>
+                  <p class="greeting-text"><b>Si Wafer와 SiC Wafer 등 다양한 Wafer 검사를 위해 순수 국내기술로 Air Pocket 검사 장비 제품군, Edge/Surface 검사 장비 제품군, 시스템 등을 개발하여 생산하고 있으며 다양한 하드웨어 제품군과 이와 관련된 다양한 소프트웨어 제품군</b>을 가지고 있습니다.</p>
+                </div>
+                <div class="greeting-text-box">
+                  <p class="greeting-text">현재 위스팩의 제품은 지속 성장하고 있는 반도체 시장에서 세계 각국의 글로벌 기업들이 웨이퍼 생산에 꼭 필요한 핵심 사업 장비로 사용되고 있습니다.</p>
+                  <p class="greeting-text"><b>Ai기술과 새로운 알고리즘 접목을 통해 순수국산의 기술로 글로벌 Top Tier 달성</b>이 목표입니다. 국산의 기술이 세계 시장을 선도할 수 있도록 따뜻한 사랑과 많은 응원 부탁드립니다.</p>
+                  <p class="greeting-text">감사합니다.</p>
+                </div>
+                <div class="greeting-sign-box">
+                  <p class="greeting-sign-text1">주식회사 위스팩</p>
+                  <p class="greeting-sign-text2">대표이사 &nbsp;민 경 훈</p>
+                </div>
+              </div>
+            </div>
+            <div class="greeting-rt">
+              <img src="/source/img/greeting-img.jpg" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </div>
