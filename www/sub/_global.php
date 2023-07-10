9cafdbf491f576cd8c218b27a6b398f678ddc6a8
onesick <onesick@iium.kr>
2023-07-10 11:47:54 +0900

    (위스팩소개>글로벌 네트워크) PC 완료

--- www/sub/_global.php
+++ www/sub/_global.php
@@ -4,4 +4,31 @@ include_once(G5_INCLUDE_PATH.'/sub_top.php');
 
 <div id="global" class="contents">
   <?php sub_top($sb_menus, 'introduce', 'global'); ?>
+
+  <div id="sb-contents">
+    <section class="global-intro">
+      <h2 class="sound_only">비즈니스 소개 영역</h2>
+      <div class="container">
+        <div class="wrapper">
+          <div class="sec_ct">
+            <div class="sb_top-title">
+              <p class="sb_top-title-text1"><b>Partner with WISFAC</b></p>
+              <p class="sb_top-title-text2">우리 회사는 고객을 만족시키기 위해 <b>전 세계에 위치한 파트너 협력사와 계약을 맺고 긴밀한 관계를 유지</b>하고 있습니다. <br/>우리의 기술이 세계로 진출할 수 있도록 최선을 다하고 있습니다. <br/>현재 아시아 지역을 넘어 곧 미국, 유럽 고객들을 만날 것입니다.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="global-map">
+      <h2 class="sound_only">글로벌 지도이미지 영역</h2>
+      <div class="container">
+        <div class="wrapper">
+          <div class="sec_ct">
+            <img src="/source/img/global-map-img.png" alt="">
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </div>
